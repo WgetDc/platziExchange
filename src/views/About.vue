1790a725bb7ca9971d28d4db23b6aadf0eeb0ee3
WgetDc <ingcorellana@gmail.com>
2020-05-04 01:11:10 -0400

    Router añadido. Funcionando la Home /, el /about y paginas de error (404)

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex-col items-center">
+        <p>
+            Soy una pagina de About. 
+        </p>
+        
+        <router-link class="mt-5 text-xl  text-green-600 hover:underline" to="/">Volver al home </router-link>
+        
+        
+    </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Error 404 mi bro! Pagina no encontrada. <router-link class="text-red-600" to="/">Mejor vuelve a la Home</router-link></h1>
+    </div>
+</template>
