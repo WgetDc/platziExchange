0f76e20ac0d9cfe6a5f2a38dc01e5fba41a5fdf7
WgetDc <ingcorellana@gmail.com>
2020-05-04 17:44:48 -0400

    API Rest funcionando. Rellenando tabla con datos

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -14,13 +14,16 @@
       </tr>
     </thead>
     <tbody>
-      <tr class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100">
+      <tr v-for="a in assets" :key="a.id"
+       class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100">
+        <td>
+          <img class="w-6 h-6" v-bind:src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`" alt="">
+        </td>
         <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{{a.name}}</td>
+        <td>{{a.priceUsd}}</td>
+        <td>{{a.marketCapUsd}}</td>
+        <td>{{a.vwap24Hr}}</td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
-        <px-assets-table />
-    </div>
+  <div>
+    <px-assets-table v-bind:assets="assets" />
+  </div>
 </template>
 
 <script>
-
 import PxAssetsTable from '@/components/PxAssetsTable'
 
-export default {
-    name: "Home",
+import api from '@/api'
 
-    components: { PxAssetsTable}
-};
+export default {
+  name: 'Home',
+
+  components: { PxAssetsTable },
+
+  data(){
+    return {
+      assets: []
+    }
+  },
+
+  created(){
+    api.getAssets()
+    .then(assets => (this.assets = assets))
+  }
+}
 </script>
